2b6b5b8ecad7a4899a78074676b622b3dc019f3a
Bitl <bitlblox@gmail.com>
2022-03-10 10:16:53 -0700

    add architecture detection

--- comp.sh
+++ comp.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-cp mgw-sdk/config .config
+CONFIGNAME=config
+INPUT=`gcc -dumpmachine`
+if [[ $INPUT == *"x86_64"* ]]; then
+  CONFIGNAME=config64
+fi
+
+echo "Using config '"$CONFIGNAME"'."
+cp mgw-sdk/$CONFIGNAME .config
 read -p "Press any key to resume after reviewing your config file..."
 # review .config
 make strip
